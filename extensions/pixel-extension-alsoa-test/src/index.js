c36fa4185ca9602bbd693a0db0378ef920be4492
vyacheslav.badaev <vyacheslav.badaev@devit.group>
2023-05-04 13:32:05 +0300

    subscribe to all standart events

--- extensions/pixel-extension-alsoa-test/src/index.js
+++ extensions/pixel-extension-alsoa-test/src/index.js
@@ -1,11 +1,13 @@
 import { register } from '@shopify/web-pixels-extension';
 
+
 register(({ configuration, analytics, browser, settings, _pixelInfo }) => {
 	// Bootstrap and insert pixel script tag here
-	const endpointBase = `https://forgotten-gdp-external-bronze.trycloudflare.com/api/events?id=${settings.accountID}`;
+	const BASE_URL = 'https://kansas-weekly-biodiversity-areas.trycloudflare.com';
+	const endpointBase = `${BASE_URL}/api/events?id=${settings.accountID}`;
 	// Sample subscribe to page view
-	analytics.subscribe('page_viewed', (event) => {
-		console.log('Page viewed2', event);
+	analytics.subscribe('all_standard_events', (event) => {
+		console.log(event.name, event);
 		browser.sendBeacon(endpointBase, JSON.stringify(event));
 	});
 });
