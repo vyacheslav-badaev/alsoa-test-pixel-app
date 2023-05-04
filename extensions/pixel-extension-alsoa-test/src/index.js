ebc9e0df75b8bb54d6c2d7e6dded32ec744577ea
vyacheslav.badaev <vyacheslav.badaev@devit.group>
2023-05-04 11:24:42 +0300

    update pixel extension link

--- extensions/pixel-extension-alsoa-test/src/index.js
+++ extensions/pixel-extension-alsoa-test/src/index.js
@@ -2,7 +2,7 @@ import { register } from '@shopify/web-pixels-extension';
 
 register(({ configuration, analytics, browser, settings, _pixelInfo }) => {
 	// Bootstrap and insert pixel script tag here
-	const endpointBase = `https://micro-nothing-rebate-hindu.trycloudflare.com/api/events?id=${settings.accountID}`;
+	const endpointBase = `https://forgotten-gdp-external-bronze.trycloudflare.com/api/events?id=${settings.accountID}`;
 	// Sample subscribe to page view
 	analytics.subscribe('page_viewed', (event) => {
 		console.log('Page viewed2', event);
