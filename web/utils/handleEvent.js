211081ed6d32f6b572a13d2a74b550ba4086ebbb
vyacheslav.badaev <vyacheslav.badaev@devit.group>
2023-05-24 09:57:57 +0300

    send in parallel

--- web/utils/handleEvent.js
+++ web/utils/handleEvent.js
@@ -8,8 +8,9 @@ export default async function handleEvent(query, body, ip) {
 	const { event, ttclid, fbclid, sclid, epik } = eventData;
 	const shop = Buffer.from(accountID, 'base64').toString('ascii');
 	const shopData = await ShopsStorage.getShopByDomain(shop);
+	const alsoaRequests = [];
 
-	console.log(event.name);
+	console.log(shop, event.name);
 
 	if (!shopData) {
 		throw new Error('Shop does not have tiktok pixel id or token');
@@ -18,38 +19,47 @@ export default async function handleEvent(query, body, ip) {
 	const alsoaEventData = getEventData(event, ip);
 
 	if (ttclid && shopData.tiktokPixelId && shopData.tiktokAccessToken) {
-		await AlsoaService.sendEvent({
+		const pendingResult =  AlsoaService.sendEvent({
 			...alsoaEventData,
 			pixel_id: shopData.tiktokPixelId,
 			token: shopData.tiktokAccessToken,
 			ttclid,
 		});
+		alsoaRequests.push(pendingResult);
 	}
 
 	if (fbclid && shopData.facebookPixelId && shopData.facebookAccessToken) {
-		await AlsoaService.sendEvent({
+		const pendingResult = AlsoaService.sendEvent({
 			...alsoaEventData,
 			pixel_id: shopData.facebookPixelId,
 			token: shopData.facebookAccessToken,
 			fbclid,
 		});
+		alsoaRequests.push(pendingResult);
 	}
 
 	if (sclid && shopData.snapchatPixelId && shopData.snapchatAccessToken) {
-		await AlsoaService.sendEvent({
+		const pendingResult = AlsoaService.sendEvent({
 			...alsoaEventData,
 			pixel_id: shopData.snapchatPixelId,
 			token: shopData.snapchatAccessToken,
 			sclid,
 		});
+		alsoaRequests.push(pendingResult);
 	}
 
 	if (epik && shopData.pinterestApiKey && shopData.pinterestAccessToken) {
-		await AlsoaService.sendEvent({
+		const pendingResult = AlsoaService.sendEvent({
 			...alsoaEventData,
 			pixel_id: shopData.pinterestApiKey,
 			token: shopData.pinterestAccessToken,
 			epik,
 		});
+		alsoaRequests.push(pendingResult);
 	}
+
+	// Send all requests in parallel
+	alsoaRequests.length && await Promise.allSettled(alsoaRequests).catch((e) => {
+		console.error('Error sending events to alsoa', e);
+	});
 }
